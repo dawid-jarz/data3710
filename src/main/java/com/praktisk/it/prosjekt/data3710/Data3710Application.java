936393a0da1b457498aa67e1ba80f9c07219d04e
dawid-jarz <dawidjarzebiak4@gmail.com>
2025-08-16 14:51:23 +0200

    repo

---
+++ src/main/java/com/praktisk/it/prosjekt/data3710/Data3710Application.java
@@ -0,0 +1,13 @@
+package com.praktisk.it.prosjekt.data3710;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data3710Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data3710Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktisk/it/prosjekt/data3710/Data3710ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktisk.it.prosjekt.data3710;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data3710ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
