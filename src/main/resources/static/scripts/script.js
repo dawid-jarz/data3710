d8b3b8f4eebb1d148c19a1d812b6d7c53d946335
dawid-jarz <dawidjarzebiak4@gmail.com>
2025-09-28 14:40:48 +0200

    Responsive navbar

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,3 +1,13 @@
 function bookTime(){
     window.open("https://system.easypractice.net/book/data3710", "_blank");
 }
+
+function toggleMenu() {
+  const menu = document.getElementById("hamburger-overlay");
+  if (menu.style.width === "100%") {
+    menu.style.width = "0";
+  } 
+  else {
+    menu.style.width = "100%";
+  }
+}
